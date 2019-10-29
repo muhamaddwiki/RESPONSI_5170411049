823e4ed710df8b41e531d0681e24c513652e3c3a
muhamaddwiki <muhamaddwiki131@gmail.com>
2019-10-29 10:23:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/responsi_5170411049/SecondActivty.kt
@@ -0,0 +1,15 @@
+package com.example.responsi_5170411049
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
